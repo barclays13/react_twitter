10eb04e3e646fe1cc3b90dd74d42cb93b08e5edf
Zubarev Sergey <barclays13@mail.ru>
2019-11-04 23:42:31 +0300

    data, heart

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+let today = new Date();
 
 const PostListItem = () => {
     return (
@@ -7,6 +8,7 @@ const PostListItem = () => {
                 Heello World!
             </span>
             <div className='d-flex justify-content-center align-items-center'>
+
                 <button 
                 type='button' 
                 className='btn-star btn-sm'>
@@ -17,7 +19,13 @@ const PostListItem = () => {
                 className='btn-trash btn-sm'>
                     <i className='fa fa-trash-o'></i>
                 </button>
-                <i className='fa fa-heart'></i>
+                <i className='fa fa-heart'
+                style={{transform:'translateX(0px)'}}></i>
+                <div
+                className='date-creat-item'
+                style={{fontSize: '16px'}}>
+                    {today.getDate()}.{today.getMonth()} {today.getHours()}:{today.getMinutes()}
+                </div>
             </div>
         </li>
     )
